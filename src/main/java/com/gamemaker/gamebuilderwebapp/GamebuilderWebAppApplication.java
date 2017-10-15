a1a4115d2fae401247b52328e9986e22fdc4e990
Sudhanshu Siddh <sudhanshu.15@gmail.com>
2017-10-15 02:47:23 -0400

    Initialize Git for Spring Boot

---
+++ src/main/java/com/gamemaker/gamebuilderwebapp/GamebuilderWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.gamemaker.gamebuilderwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebuilderWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebuilderWebAppApplication.class, args);
+	}
+}
