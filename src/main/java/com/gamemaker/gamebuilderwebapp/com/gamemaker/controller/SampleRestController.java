107cae324f266a280c29a103f55b6bfe3c27d93d
Sudhanshu Siddh <sudhanshu.15@gmail.com>
2017-10-15 05:43:47 -0400

    F2017A7T4-13 - http://tintin.cs.indiana.edu:8091/browse/F2017A7T4-13 - Modified code to display data using bootstrap and thymeleaf in tabular format

--- src/main/java/com/gamemaker/gamebuilderwebapp/com/gamemaker/controller/SampleRestController.java
+++ src/main/java/com/gamemaker/gamebuilderwebapp/com/gamemaker/controller/SampleRestController.java
@@ -3,25 +3,31 @@ package com.gamemaker.gamebuilderwebapp.com.gamemaker.controller;
 import com.gamemaker.gamebuilderwebapp.com.gamemaker.model.Score;
 import com.gamemaker.gamebuilderwebapp.com.gamemaker.repository.GamesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class SampleRestController {
 
     @Autowired
     GamesRepository gamesRepository;
 
     @GetMapping("/")
-    public String hello() {
-        return "Hello World!";
+    public String hello(Model model) {
+        String greet = "Hello World";
+        model.addAttribute("greeting", greet);
+        return "index";
     }
 
     @GetMapping("/games")
-    public List<Score> getAll() {
-        return gamesRepository.findAll();
+    public String getAll(Model model) {
+        List<Score> scores = gamesRepository.findAll();
+        model.addAttribute("scoreslist", scores);
+        return "scoressheet";
     }
 
 }
